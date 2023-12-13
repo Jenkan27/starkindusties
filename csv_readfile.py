1bf1151e36c86f6dcc5f705ac60394f8a75ddef4
Jenkan27 <holmjens84@gmail.com>
2023-12-13 10:16:29 +0100

    byggt en csv reader list skapare grej

---
+++ csv_readfile.py
@@ -0,0 +1,27 @@
+from models import Sale
+import csv
+
+list_from_file = []
+
+with open("100SalesRecords.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    
+    first_row = True
+    for row in csvreader:
+        if first_row:
+            row_title = []
+            for element in row:
+                row_title.append(element)
+            first_row = False
+            continue
+        row_dict = {}
+        index = 0
+        
+        while index < len(row):
+            for element in row:
+                row_dict[row_title[index]] = element
+                index += 1
+        list_from_file.append(row_dict)
+
+for line in list_from_file:
+    
