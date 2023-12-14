ed897bb8344f63f8a1ad63a8c9521dfe516427f5
Daniel Alm D'Adda <swedmarine@hotmail.com>
2023-12-14 11:10:05 +0100

    Packaged into class and prepared infrastructure for bigger files.

--- csv_readfile.py
+++ csv_readfile.py
@@ -1,60 +1,83 @@
-from app import app, db
-from models import Sale
-from datetime import date, datetime
 import csv
+from app import app, db
+from datetime import date, datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models import Sale
 
-def convert_american_to_date(american_date: str) -> date:
-    month = american_date.split('/')[0]
-    day = american_date.split('/')[1]
-    year = american_date.split('/')[2]
-    
-    iso_date = year
-    if len(month) < 2:
-        month = '0' + month
-    iso_date += month
-    if len(day) < 2:
-        day ='0' + day
-    iso_date += day
-    
-    converted_date  = date.fromisoformat(iso_date)
-    return converted_date
 
-def import_from_csv(file) -> None:
-    with open(file, 'r') as file:
-        csvreader = csv.DictReader(file, delimiter=',')
-        with app.app_context():
-            count = 0
-            for row in csvreader:
-                if row['Region'] != 'Region':
-                    sale = Sale()
-                    sale.region = row['Region']
-                    sale.country = row['Country']
-                    sale.item_type = row['Item Type']
-                    sale.sales_channel = row['Sales Channel']
-                    sale.order_priority = row['Order Priority']
-                    sale.order_date = convert_american_to_date(row['Order Date'])
-                    sale.order_id = int(row['Order ID'])
-                    sale.ship_date = convert_american_to_date(row['Ship Date'])
-                    sale.units_sold = int(row['Units Sold'])
-                    sale.unit_price = float(row['Unit Price'])
-                    sale.unit_cost = float(row['Unit Cost'])
-                    sale.total_revenue = float(row['Total Revenue'])
-                    sale.total_cost = float(row['Total Cost'])
-                    sale.total_profit = float(row['Total Profit'])
-                    db.session.add(sale)
-                count += 1
-                if count % 50000 == 0:
-                    timestamp = datetime.now()
-                    print(f'[{timestamp}]: {count} rows processed...')
+class DataBaseLoader:
+    def __init__(self, app: Flask, db: SQLAlchemy) -> None:
+        self._app = app
+        self._db = db
+
+    def csv_to_database(self, csv_file: str) -> None:
+        with open(csv_file, 'r') as file:
+            csv_reader = csv.DictReader(file, delimiter=',')
+            print(f'CSV file "{csv_file}" loaded.')
+            row_count = 0
+            for row in csv_reader:
+                row_count += 1
+            print(f'Detecting {row_count} entries.')
+            file.seek(0)
+            if row_count <= 10000:
+                count = 0
+                for row in csv_reader:
+                    if row['Region'] == 'Region':
+                        continue
+                    self._create_sale(row)
+                    count += 1
+                    if count % 100 == 0:
+                        timestamp = datetime.now()
+                        print(f'[{timestamp}]: {count} rows processed...')
+            else:
+                print('File too big.')
+    
+    def _create_sale(self, data: dict[str, str]) -> None:
+        with self._app.app_context():
+            sale = Sale()
+            sale.region = data['Region']
+            sale.country = data['Country']
+            sale.item_type = data['Item Type']
+            sale.sales_channel = data['Sales Channel']
+            sale.order_priority = data['Order Priority']
+            sale.order_date = self._convert_american_to_date(data['Order Date'])
+            sale.order_id = int(data['Order ID'])
+            sale.ship_date = self._convert_american_to_date(data['Ship Date'])
+            sale.units_sold = int(data['Units Sold'])
+            sale.unit_price = float(data['Unit Price'])
+            sale.unit_cost = float(data['Unit Cost'])
+            sale.total_revenue = float(data['Total Revenue'])
+            sale.total_cost = float(data['Total Cost'])
+            sale.total_profit = float(data['Total Profit'])
+            db.session.add(sale)
             db.session.commit()
 
+    def _convert_american_to_date(self, american_date: str) -> date:
+        month = american_date.split('/')[0]
+        day = american_date.split('/')[1]
+        year = american_date.split('/')[2]
+        
+        iso_date = year
+        if len(month) < 2:
+            month = '0' + month
+        iso_date += month
+        if len(day) < 2:
+            day ='0' + day
+        iso_date += day
+        
+        converted_date  = date.fromisoformat(iso_date)
+        return converted_date
+                        
+
 if __name__ == '__main__':
+    db_loader = DataBaseLoader(app, db)
     opt = input('Import 100 or 5m? ')
     start = datetime.now()
     if opt == '5m':
-        import_from_csv('5mSalesRecords.csv')
+        db_loader.csv_to_database('5mSalesRecords.csv')
     elif opt == '100':
-        import_from_csv('100SalesRecords.csv')
+        db_loader.csv_to_database('100SalesRecords.csv')
     else:
         print('Unrecognised option. No import.')
         quit()
